34a2ac37e3eae52b845fc85f69333d552e603d82
SizzlRetrat <103748542+SizzlRetrat@users.noreply.github.com>
2022-04-23 13:39:28 +0100

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,23 @@
+print 'hello, this is the python version of im not a goat.html, here it is: you know that im not a goat. 
+ you are an idiot if you think that 
+here's proof: https://youtu.be/HB2kvOq8S2I 
+now watch the video till the end, ore youre done for
+check xpqernie giveaway, its coming soon
+tysm for downloading! luv u guys   
+Waaiiit, how u find my github 
+ oh now i get it u searched my name
+still confused, why u searched my name?    
+ oh u wierdo, my vid hasnt come out, wait till its out
+im coding an app, the vids out soon =)    
+did you know that this app is 45MB 
+thats the reason why im here 
+ im still here so i can waste ur memory
+now u regret finding it
+enjoy this
+lllllllllllllllllllllllllllllllllllllllllllllllllllll
+ooooooooooooooooooooooooooooooooooooooooooooooooooooo
+lllllllllllllllllllllllllllllllllllllllllllllllllllll
+check out all my websites!
+https://heblen.c1.biz https://hcard.c1.biz
+and https://exbos.c1.biz
+ok bye'
