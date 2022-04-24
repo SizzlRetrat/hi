60bfa4304ed094779e8a62fb2ed108cac4255983
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2022-04-24 10:04:10 +0100

    Create 75 5.py

---
+++ 75 5.py
@@ -0,0 +1,11 @@
+print '
+auto hot key script i like beds ok bye
+how are ya because im good
+aml iso very doofus cosmp how r ya lol wtf are ya doin
+the python iso seventy five point five so im trying to get 
+that very high ando hopefully can get an app foro this
+ifo you are wondering why some letters have an o on the end
+its because they are CRITICAL python names so we have to
+add an o ando just ignore the o aso we cant use them oro
+mess around witho the python settings
+'
