c5fe5b0194cc70842092a3bf2b26a088b98468ef
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2022-04-24 11:18:42 +0100

    Create Alert stuff.js

---
+++ Alert stuff.js
@@ -0,0 +1,9 @@
+alert ("thanks for using javascript");
+alert ("MaxFone Max");
+var Lol = hello;
+alert ("Lol");
+var xpoernie = xpoernie is a giveaway that happned innovember twenty twenty one, some have an o on tge end that is because they are js reserverd;
+alert ("xpoernie");
+var java = fs javascript;
+alert ("js is javascript and there is a variable for it and it is javascript without scripy and it is java;
+       
